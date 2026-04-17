2cbf8ddf3f9a1c85bbadcedae9b74848e465c195
Shubham Sinha <shubhamsinha2009@gmail.com>
2026-04-17 15:22:57 +0000

    fix: trigger CarUnplugged event during EvseReplugStarted to ensure proper state transition

--- modules/EVSE/EvseManager/Charger.cpp
+++ modules/EVSE/EvseManager/Charger.cpp
@@ -961,6 +961,7 @@ void Charger::process_cp_events_state(CPEvent cp_event) {
 void Charger::process_cp_events_independent(CPEvent cp_event) {
     switch (cp_event) {
     case CPEvent::EvseReplugStarted:
+        process_cp_events_independent(CPEvent::CarUnplugged);
         shared_context.current_state = EvseState::Replug;
         break;
     case CPEvent::EvseReplugFinished:
